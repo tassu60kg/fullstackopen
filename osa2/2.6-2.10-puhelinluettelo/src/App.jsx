72b6fde7e89fb58cddbdf99cee318a64dec0319a
tassu60kg <159922849+tassu60kg@users.noreply.github.com>
2026-02-16 17:12:47 +0200

    lisaa tehtava 2.11

--- osa2/2.6-2.10-puhelinluettelo/src/App.jsx
+++ osa2/2.6-2.10-puhelinluettelo/src/App.jsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
+import { useState, useEf, useEffect } from 'react'
 import Form from './components/form'
 import Persons from './components/persons'
 import Filter from './components/filter'
-
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
    
-
+  useEffect(() => {
+    console.log('effect')
+    axios
+        .get('http://localhost:3001/persons')
+        .then(response => {
+          console.log('promise fulfilled')
+          setPersons(response.data)
+        })
+  }, [])
   return (
     <div>
       <h2>Phonebook</h2>
--- osa2/part2/src/App.jsx
+++ osa2/part2/src/App.jsx
@@ -1,51 +1,56 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('') 
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
-const addNote = (event) => {
-  event.preventDefault()
-  const noteObject = {
-    content: newNote,
-    important: Math.random() > 0.5,
-    id: String(notes.length + 1),
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/notes').then((response) => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+  }, [])
+  console.log('render', notes.length, 'notes')
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: String(notes.length + 1),
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
 
-  setNotes(notes.concat(noteObject))
-  setNewNote('')
-}
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
 
-const handleNewNote = (event) => {
-  console.log(event.target.value)
-  setNewNote(event.target.value)
-}
-
-const notesToShow = showAll 
-  ? notes
-  : notes.filter(note => note.important)
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
-        <div>
-          <button onClick={() => setShowAll(!showAll)}>
-            show {showAll ? 'important' : 'all' }
-          </button>
-        </div>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notesToShow.map(note => 
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
-        )}
+        ))}
       </ul>
       <form onSubmit={addNote}>
-
-        <input value={newNote} 
-        onChange={handleNewNote}/>
+        <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
-      </form>   
+      </form>
     </div>
   )
 }
